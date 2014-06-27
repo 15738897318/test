a4f7edbf3c498f7b2db61b1dbe6d48936f2a5649
Bao Nguyen <storm5906@gmail.com>
2014-06-27 14:01:31 +0700

    + Fix rgb2tsl() and add rgb2ntsc(), ntsc2rgb(), buildGdownStack(), idealBandpassing() interfaces.

--- MisfitHeartRate/MisfitHeartRate/algorithms/image/MHRImage.h
+++ MisfitHeartRate/MisfitHeartRate/algorithms/image/MHRImage.h
@@ -13,9 +13,29 @@
 #include "MHRMatrix.h"
 
 namespace cv {
-
-    // convert a RGB Mat to a TSL Mat
-    Mat rgb2tsl(const Mat& srcRGBmap);
+	// convert a RGB Mat to a TSL Mat
+	Mat rgb2tsl(const Mat& srcRGBmap);
+    
+	// convert a RGB Mat to a NTSC Mat
+	Mat rgb2ntsc(const Mat& srcRGBmap);
+    
+	// convert a RGB Mat to a NTSC Mat
+	Mat ntsc2rgb(const Mat& srcNTSCmap);
+    
+	// Apply Gaussian pyramid decomposition on VID_FILE from START_INDEX to END_INDEX
+	// and select a specific band indicated by LEVEL.
+	// GDOWN_STACK: stack of one band of Gaussian pyramid of each frame
+	// the first dimension is the time axis
+	// the second dimension is the y axis of the video
+	// the third dimension is the x axis of the video
+	// the forth dimension is the color channel
+	Mat buildGDownStack(String vidFile, int startIndex, int endIndex, int level);
+    
+	// Apply ideal band pass filter on INPUT along dimension DIM.
+	// WL: lower cutoff frequency of ideal band pass filter
+	// WH: higher cutoff frequency of ideal band pass filter
+	// SAMPLINGRATE: sampling rate of INPUT
+	Mat idealBandpassing(Mat input, int dim, double wl, double wh, double samplingRate);
     
     // convert a frame to signal
     void frames2signal(Mat monoframes, String conversion_method, double frameRate, double cutoff_freq,
