b2bc5ecb8175a56a45409200cd19d78e303e5acb
Bao Nguyen Le <storm5906@gmail.com>
2014-07-05 08:59:11 +0700

    + Update rgb2ntsc() and ntsc2rbg(), clip results with range [0, 1].

--- MisfitHeartRate/MisfitHeartRate/algorithms/image/image.cpp
+++ MisfitHeartRate/MisfitHeartRate/algorithms/image/image.cpp
@@ -16,8 +16,29 @@ namespace MHR {
         printf("Save a frame to %s\n", outFile.c_str());
         return imwrite(outFile, frame);
     }
-    
-    
+
+
+    // multiply each pixel of a frame with a base matrix
+    // and clip the result's values by range [lower_bound, upper_bound]
+    Mat mulAndClip(const Mat &frame, const Mat &base, double lower_bound, double upper_bound) {
+        Mat ans;
+        frame.convertTo(ans, CV_64FC3);
+        int nChannel = ans.channels();
+        MatIterator_<Vec3d> it = ans.begin<Vec3d>();
+        for (int i = 0; i < ans.rows; ++i)
+            for (int j = 0; j < ans.cols; ++j) {
+                Mat tmp = base * Mat(*it);
+                MatIterator_<double> tmp_it = tmp.begin<double>();
+                for (int channel = 0; channel < nChannel; ++channel) {
+                    *tmp_it = min(*tmp_it, upper_bound);
+                    *tmp_it++ = max(*tmp_it, lower_bound);
+                }
+                *it++ = Vec3d(tmp);
+            }
+        return ans;
+    }
+
+
 	// convert a RGB Mat to a TSL Mat
 	Mat rgb2tsl(const Mat& srcRGBmap)
 	{
@@ -30,13 +51,13 @@ namespace MHR {
 		Mat r_primes = Mat::zeros(nRow, nCol, CV_64F);
 		divide(cloneWithChannel(rbgmap, 0), sumChannels(rbgmap), r_primes);
 		subtract(r_primes, Mat(nRow, nCol, CV_64F, cvScalar(1.0/3.0)), r_primes);
-		
+
 //        g_primes = bsxfun(@minus, bsxfun(@rdivide, rgbmap(:, :, 2), sum(rgbmap, 3)), 1/3);
 //        g_primes(isnan(g_primes)) = -1/3;
 		Mat g_primes = Mat::zeros(nRow, nCol, CV_64F);
 		divide(cloneWithChannel(rbgmap, 1), sumChannels(rbgmap), g_primes);
 		subtract(r_primes, Mat(nRow, nCol, CV_64F, cvScalar(1.0/3.0)), g_primes);
-		
+
 //        temp1 = zeros(size(g_primes));
 //        temp1(bsxfun(@gt, g_primes, 0)) = 1/4;
 //        temp1(bsxfun(@lt, g_primes, 0)) = 3/4;
@@ -58,7 +79,7 @@ namespace MHR {
 				{
 					temp2.at<double>(i, j) = 0;
 				}
-		
+
 		Mat tslmap = Mat::zeros(nRow, nCol, CV_64FC3);
 //        tslmap(:, :, 1) = 1 / (2 * pi) * bsxfun(@atan2, r_primes, g_primes) .* temp2 + temp1;
 		Mat tmp0 = atan2Mat(r_primes, g_primes);
@@ -83,60 +104,53 @@ namespace MHR {
 			}
 		return tslmap;
 	}
-    
-    
-	// convert a RGB Mat to a NTSC Mat
+
+
+    // convert a RGB Mat to a NTSC Mat
     // ref: http://en.wikipedia.org/wiki/YIQ
-	Mat rgb2ntsc(const Mat& rgbFrame) {
-        int rows = rgbFrame.rows, cols = rgbFrame.cols;
-        double baseArray[9] = {
+    //      http://www.mathworks.com/help/images/ref/rgb2ntsc.html
+    Mat rgb2ntsc(const Mat& rgbFrame) {
+        /*double baseArray[9] = {
             0.299, 0.587, 0.114,
             0.595716, -0.274453, -0.321263,
             0.211456, -0.522591, 0.311135,
-        };
-        Mat base = arrayToMat(baseArray, 3, 3);
-        // calculate result Mat
-        Mat ans = Mat::zeros(rows, cols, CV_64FC3);
-        Mat tmp = Mat::zeros(3, 1, CV_64F);
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j) {
-                for (int channel = 0; channel < 3; ++channel)
-                    tmp.at<double>(channel, 0) = rgbFrame.at<Vec3d>(i, j)[channel];
-                //                printf("%f %f %f\n", tmp.at<double>(0, 0), tmp.at<double>(1, 0), tmp.at<double>(2, 0));
-                tmp = base * tmp;
-                //                printf("---> %f %f %f\n", tmp.at<double>(0, 0), tmp.at<double>(1, 0), tmp.at<double>(2, 0));
-                for (int channel = 0; channel < 3; ++channel)
-                    ans.at<Vec3d>(i, j)[channel] = tmp.at<double>(channel, 0);
-            }
-		return ans;
-	}
-    
-    
-	// convert a RGB Mat to a NTSC Mat
-    // ref: http://en.wikipedia.org/wiki/YIQ
-	Mat ntsc2rgb(const Mat& ntscFrame)	{
-        int rows = ntscFrame.rows, cols = ntscFrame.cols;
+        };*/
         double baseArray[9] = {
-            1, 0.9563, 0.6210,
-            1, -0.2721, -0.6474,
-            1, -1.1070, 1.7046,
+            0.299, 0.587, 0.114,
+            0.596, -0.274, -0.322,
+            0.211, -0.523, 0.312,
         };
         Mat base = arrayToMat(baseArray, 3, 3);
-        // calculate result Mat
-		Mat ans = Mat::zeros(rows, cols, CV_64FC3);
-        Mat tmp = Mat::zeros(3, 1, CV_64F);
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j) {
-                for (int channel = 0; channel < 3; ++channel)
-                    tmp.at<double>(channel, 0) = ntscFrame.at<Vec3d>(i, j)[channel];
-                tmp = base * tmp;
-                for (int channel = 0; channel < 3; ++channel)
-                    ans.at<Vec3d>(i, j)[channel] = tmp.at<double>(channel, 0);
-            }
-		return ans;
-	}
-    
-    
+        return mulAndClip(rgbFrame, base, 0, 1);
+    }
+
+
+    // convert a RGB Mat to a NTSC Mat
+    // ref: http://en.wikipedia.org/wiki/YIQ
+    //      http://www.mathworks.com/help/images/ref/ntsc2rgb.html
+    Mat ntsc2rgb(const Mat& ntscFrame)  {
+        clock_t startTime = clock();
+        int rows = ntscFrame.rows, cols = ntscFrame.cols;
+        //double baseArray[9] = {
+        //  1, 0.9563, 0.6210,
+        //  1, -0.2721, -0.6474,
+        //  1, -1.1070, 1.7046,
+        //};
+        double baseArray[9] = {
+            1.0, 0.956, 0.621,
+            1.0, -0.272, -0.647,
+            1.0, -1.106, 1.703,
+        };
+        //double baseArray[9] = {
+        //    1.0000,    1.0000,    1.0000,
+        //  0.9560,   -0.2720,   -1.1060,
+        //  0.6210,   -0.6470,    1.7030,
+        //};
+        Mat base = arrayToMat(baseArray, 3, 3);
+        return mulAndClip(ntscFrame, base, 0, 1);
+    }
+
+
     // Blur and downsample an image.  The blurring is done with
     // filter kernel specified by FILT (default = 'binom5')
     Mat blurDnClr(const Mat& src, int level) {
@@ -145,8 +159,8 @@ namespace MHR {
             pyrDown(ans, ans, Size(ans.cols/2, ans.rows/2));
         return ans;
     }
-    
-    
+
+
     // Compute correlation of matrices IM with FILT, followed by
     // downsampling.  These arguments should be 1D or 2D matrices, and IM
     // must be larger (in both dimensions) than FILT.  The origin of filt
@@ -164,8 +178,8 @@ namespace MHR {
                 ans.at<double>(i, j) = tmp.at<double>(x, y);
         return ans;
     }
-    
-	
+
+
 	// Apply Gaussian pyramid decomposition on VID_FILE from START_INDEX to END_INDEX
 	// and select a specific band indicated by LEVEL.
 	// GDOWN_STACK: stack of one band of Gaussian pyramid of each frame
@@ -178,45 +192,45 @@ namespace MHR {
 //		int vidHeight = vid[0].rows;
 //		int vidWidth = vid[0].cols;
 //		int nChannels = vid[0].channels(); // 3 ????
-        
+
         // firstFrame
         Mat frame = vid[0];
         Mat rgbframe = convertTo(frame, CV_64FC3);
         frame = rgb2ntsc(rgbframe);
-        
+
         frameToFile(vid[0], "/var/mobile/Applications/40BBE745-97D5-4BEA-B486-AB77BCE9B3B2/Documents/test_frame_rgb2ntsc.jpg");
-        
+
         // Blur and downsample the frame
         Mat blurred = blurDnClr(frame, level);
-        
+
         printf("blurred.size = (%d, %d)\n", blurred.rows, blurred.cols);
-        
+
         frameToFile(blurred, "/var/mobile/Applications/40BBE745-97D5-4BEA-B486-AB77BCE9B3B2/Documents/test_frame_blurred.jpg");
-        
+
         // create pyr stack
         // Note that this stack is actually just a SINGLE level of the pyramid
         int GdownSize[] = {endIndex - startIndex + 1, blurred.size.p[0], blurred.size.p[1]};
 		Mat GDownStack = Mat(3, GdownSize, CV_64FC3, cvScalar(0));
-        
+
         printf("GdownSize: (%d, %d, %d)\n", GDownStack.size.p[0], GDownStack.size.p[1], GDownStack.size.p[2]);
-        
+
         // The first frame in the stack is saved
         for (int i = 0; i < GDownStack.size.p[1]; ++i)
             for (int j = 0; j < GDownStack.size.p[2]; ++j)
                 for (int t = 0; t < 3; ++t)
                     GDownStack.at<Vec3d>(0, i, j)[t] = blurred.at<Vec3d>(i, j)[t];
-        
+
         for (int i = startIndex+1, k = 1; i <= endIndex; ++i, ++k) {
             // Create a frame from the ith array in the stream
             frame = vid[i];
             rgbframe = convertTo(frame, CV_64FC3);
             frame = rgb2ntsc(rgbframe);
-            
+
 //            printf("buildGDownStack: %d --> %d\n", i, endIndex);
-            
+
             // Blur and downsample the frame
             blurred = blurDnClr(frame, level);
-            
+
             // The kth element in the stack is saved
             // Note that this stack is actually just a SINGLE level of the pyramid
             for (int i = 0; i < GDownStack.size.p[1]; ++i)
--- MisfitHeartRate/MisfitHeartRate/algorithms/image/image.h
+++ MisfitHeartRate/MisfitHeartRate/algorithms/image/image.h
@@ -14,34 +14,39 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/imgproc/types_c.h>
+#include "time.h"
 #include "matrix.h"
 
 
 namespace MHR {
     // print a frame to file
     bool frameToFile(const Mat& frame, const String& outFile);
-    
+
+    // multiply each pixel of a frame with a base matrix
+    // and clip the result's values by range [lower_bound, upper_bound]
+    Mat mulAndClip(const Mat &frame, const Mat &base, double lower_bound, double upper_bound);
+
 	// convert a RGB Mat to a TSL Mat
 	Mat rgb2tsl(const Mat& srcRGBmap);
-    
+
 	// convert a RGB Mat to a NTSC Mat
     // ref: http://en.wikipedia.org/wiki/YIQ
 	Mat rgb2ntsc(const Mat& rgbFrame);
-    
+
 	// convert a RGB Mat to a NTSC Mat
     // ref: http://en.wikipedia.org/wiki/YIQ
 	Mat ntsc2rgb(const Mat& ntscFrame);
-    
+
     // Blur and downsample an image.  The blurring is done with
     // filter kernel specified by FILT (default = 'binom5')
     Mat blurDnClr(const Mat& src, int level);
-    
+
     // Compute correlation of matrices IM with FILT, followed by
     // downsampling.  These arguments should be 1D or 2D matrices, and IM
     // must be larger (in both dimensions) than FILT.  The origin of filt
     // is assumed to be floor(size(filt)/2)+1.
     Mat corrDn(const Mat &src, const Mat &filter, int rectRow, int rectCol);
-    
+
 	// Apply Gaussian pyramid decomposition on VID_FILE from START_INDEX to END_INDEX
 	// and select a specific band indicated by LEVEL.
 	// GDOWN_STACK: stack of one band of Gaussian pyramid of each frame
