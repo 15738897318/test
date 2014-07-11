f6c7d05f7f92ef49f60fec39a536cf0ddd6b2a4c
Bao Nguyen <storm5906@gmail.com>
2014-07-11 11:20:16 +0700

    + Add some functions to easily change position of a UIView and check iOS version of device.

---
+++ MisfitHeartRate/MisfitHeartRate/Utilities/UIView+UIView_Position.h
@@ -0,0 +1,13 @@
+//
+//  UIView+UIView_Position.h
+//  MisfitHeartRate
+//
+//  Created by Bao Nguyen on 7/11/14.
+//  Copyright (c) 2014 misfit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (UIView_Position)
+
+@end
