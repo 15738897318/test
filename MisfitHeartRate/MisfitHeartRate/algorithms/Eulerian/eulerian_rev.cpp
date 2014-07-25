2768db5c1d57cc09789c217711b3aa25329293af
Tuan-Anh Tran <tuan-anh.tran@balliol.oxon.org>
2014-07-25 15:04:57 +0700

    Attempts on recoding to use only 1 video channel in the algorithm

---
+++ MisfitHeartRate/MisfitHeartRate/algorithms/Eulerian/eulerian_rev.cpp
@@ -0,0 +1,183 @@
+//
+//  eulerian.cpp
+//  MisfitHeartRate
+//
+//  Created by Bao Nguyen on 7/3/14.
+//  Copyright (c) 2014 misfit. All rights reserved.
+//
+
+#include "eulerian.h"
+
+
+namespace MHR {
+	// Spatial Filtering: Gaussian blur and down sample
+	// Temporal Filtering: Ideal bandpass
+    void eulerianGaussianPyramidMagnification(const vector<Mat> &vid, vector<Mat> &ans,
+                                          String outDir, double alpha, int level,
+                                          double freqBandLowEnd, double freqBandHighEnd,
+                                          double samplingRate, double chromAttenuation)
+	{
+        clock_t t1 = clock();
+
+        ans.clear();        
+		// Extract video info
+		int vidHeight = vid[0].rows;
+		int vidWidth = vid[0].cols;
+		int nChannels = vid[0].channels(); //_number_of_channels;
+		int frameRate = _frameRate;             // Can not get it from vidIn!!!! :((
+		int len = (int)vid.size();
+        
+        if (DEBUG_MODE) {
+            printf("width = %d, height = %d\n", vidWidth, vidHeight);
+            printf("frameRate = %d, len = %d\n", frameRate, len);
+            frameToFile(vid[0], outDir + "test_frame_in.jpg");
+        }
+      
+		samplingRate = frameRate;
+		level = min(level, (int)floor(log(min(vidHeight, vidWidth) / _Gpyr_filter_length) / log(2)));
+        
+		// Define the indices of the frames to be processed
+		int startIndex = _startFrame;
+        int endIndex = len - 1;
+        if (_endFrame > 0)
+            endIndex = min(endIndex, _endFrame);
+        else
+            endIndex = max(0, endIndex + _endFrame);
+        
+		// ================= Core part of the algo described in literature
+		// compute Gaussian blur stack
+		// This stack actually is just a single level of the pyramid
+        if (DEBUG_MODE) printf("Spatial filtering...\n");
+		vector<Mat> GdownStack;
+        build_Gdown_Stack(vid, GdownStack, startIndex, endIndex, level);
+		if (DEBUG_MODE) printf("Finished\n");
+        
+		// Temporal filtering
+		if (DEBUG_MODE) printf("Temporal filtering...\n");
+        vector<Mat> filteredStack;
+        ideal_bandpassing(GdownStack, filteredStack, freqBandLowEnd, freqBandHighEnd, samplingRate);
+//        filter_bandpassing(GdownStack, filteredStack);
+		if (DEBUG_MODE) printf("Finished\n");
+        
+        
+		// amplify
+        int nTime = (int)filteredStack.size();
+        int nRow = filteredStack[0].rows;
+        int nCol = filteredStack[0].cols;
+        if (THREE_CHAN_MODE | DEBUG_MODE) {
+			Mat base_B = (Mat_<double>(3, 3) <<
+						  alpha, 0, 0,
+						  0, alpha*chromAttenuation, 0,
+						  0, 0, alpha*chromAttenuation);
+			Mat base_C = (ntsc2rgb_baseMat * base_B) * rgb2ntsc_baseMat;
+			Mat tmp = Mat::zeros(nChannels, nCol, CV_64F);
+        	
+			for (int t = 0; t < nTime; ++t) {
+				for (int i = 0; i < nRow; ++i) {
+					for (int j = 0; j < nCol; ++j)
+						for (int channel = 0; channel < nChannels; ++channel)
+							tmp.at<double>(channel, j) = filteredStack[t].at<Vec3d>(i, j)[channel];
+							
+					tmp = base_C * tmp;
+					
+					for (int j = 0; j < nCol; ++j)
+						for (int channel = 0; channel < nChannels; ++channel)
+							filteredStack[t].at<Vec3d>(i, j)[channel] = tmp.at<double>(channel, j);
+					}
+				}
+			}
+		else {
+			for (int t = 0; t < nTime; ++t) {
+				for (int i = 0; i < nRow; ++i) {
+					for (int j = 0; j < nCol; ++j)
+						for (int channel = 0; channel < nChannels; ++channel)
+							tmp.at<double>(channel, j) = filteredStack[t].at<double>(i, j);
+					
+					tmp = alpha * tmp; // Only correct for chromAtn == 1
+					
+					for (int j = 0; j < nCol; ++j)
+						for (int channel = 0; channel < nChannels; ++channel)
+							filteredStack[t].at<double>(i, j) = tmp.at<double>(channel, j);
+					}
+				}
+        	}
+        
+		// =================
+        
+		// Render on the input video
+		if (DEBUG_MODE) printf("Rendering...\n");
+		// output video
+		// Convert each frame from the filtered stream to movie frame
+        Mat frame, filtered;
+        if (THREE_CHAN_MODE | DEBUG_MODE)
+        	Mat tmp_filtered = Mat::zeros(nRow, nCol, CV_64FC3);
+        else
+        	Mat tmp_filtered = Mat::zeros(nRow, nCol, CV_64F);
+        	
+		if (THREE_CHAN_MODE | DEBUG_MODE) {
+			for (int i = startIndex, k = 0; i <= endIndex && k < nTime; ++i, ++k) {
+            	// Reconstruct the frame from pyramid stack
+				// by removing the singleton dimensions of the kth filtered array
+				// since the filtered stack is just a selected level of the Gaussian pyramid
+				for (int x = 0; x < nRow; ++x)
+					for (int y = 0; y < nCol; ++y)
+						tmp_filtered.at<Vec3d>(x, y) = filteredStack[k].at<Vec3d>(x, y);
+			
+				// Format the image to the right size
+				resize(tmp_filtered, filtered, cvSize(vidWidth, vidHeight), 0, 0, INTER_CUBIC);
+			
+				// Convert the ith frame in the video stream to RGB (double-precision) image
+				vid[i].convertTo(frame, CV_64FC3);
+				
+				// Add the filtered frame to the original frame
+				filtered = filtered + frame;
+            
+				for (int i = 0; i < vidHeight; ++i)
+					for (int j = 0; j < vidWidth; ++j) {
+						for (int channel = 0; channel < nChannels; ++channel) {
+							double tmp = filtered.at<Vec3d>(i, j)[channel];
+							
+							tmp = min(tmp, 255.0);
+							tmp = max(tmp, 0.0);
+							
+							filtered.at<Vec3d>(i, j)[channel] = tmp;
+						}
+					} 
+				} 
+        else {
+            for (int i = startIndex, k = 0; i <= endIndex && k < nTime; ++i, ++k) {
+            	// Reconstruct the frame from pyramid stack
+				// by removing the singleton dimensions of the kth filtered array
+				// since the filtered stack is just a selected level of the Gaussian pyramid
+				for (int x = 0; x < nRow; ++x)
+					for (int y = 0; y < nCol; ++y)
+						tmp_filtered.at<double>(x, y) = filteredStack[k].at<double>(x, y);
+			
+				// Format the image to the right size
+				resize(tmp_filtered, filtered, cvSize(vidWidth, vidHeight), 0, 0, INTER_CUBIC);
+			
+				// Convert the ith frame in the video stream to RGB (double-precision) image
+				vid[i].convertTo(frame, CV_64F);
+				
+				// Add the filtered frame to the original frame
+				filtered = filtered + frame;
+            	
+				for (int i = 0; i < vidHeight; ++i)
+					for (int j = 0; j < vidWidth; ++j) {
+						for (int channel = 0; channel < nChannels; ++channel) {
+							double tmp = filtered.at<double>(i, j);
+							
+							tmp = min(tmp, 255.0);
+							tmp = max(tmp, 0.0);
+							
+							filtered.at<double>(i, j) = tmp;
+						}
+					}
+            	}
+            }
+            ans.push_back(filtered.clone());
+		}
+        if (DEBUG_MODE)
+            printf("eulerianGaussianPyramidMagnification() time = %f\n", ((float)clock() - (float)t1)/CLOCKS_PER_SEC);
+	}
+}
---
+++ MisfitHeartRate/MisfitHeartRate/algorithms/matrix/matrix_rev.cpp
@@ -0,0 +1,180 @@
+//
+//  matrix.cpp
+//  MisfitHeartRate
+//
+//  Created by Bao Nguyen on 7/3/14.
+//  Copyright (c) 2014 misfit. All rights reserved.
+//
+
+#include "matrix.h"
+
+
+namespace MHR {
+	// return a vector of integer from a to b with specific step
+	vector<int> vectorRange(int a, int b, int step) {
+		vector<int> ans;
+		for (int i = a; i <= b; i += step)
+			ans.push_back(i);
+		return ans;
+	}
+    
+    
+    // import data from a array to a Mat
+    Mat arrayToMat(const double a[], int rows, int cols) {
+		Mat ans = Mat::zeros(rows, cols, CV_64F);
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                ans.at<double>(i, j) = a[i*cols + j];
+        return ans;
+    }
+    
+    
+    // vector to Mat
+    Mat vectorToMat(const vector<double>& arr){
+        int sz = (int)arr.size();
+        Mat ans = Mat::zeros(1, sz, CV_64F);
+        for(int i = 0; i < sz; ++i)
+            ans.at<double>(0, i) = arr[i];
+        return ans;
+    }
+    
+    // Mat to vector 1D (just get the first row)
+    vector<double> matToVector1D(const Mat &m) {
+        vector<double> arr;
+        for(int i = 0; i < m.cols; ++i)
+            arr.push_back(m.at<double>(0, i));
+        return arr;
+    }
+
+
+    // read frames from a VideoCapture to a vector<Mat>
+    // return true if endOfFile
+    bool videoCaptureToVector(VideoCapture &src, vector<Mat> &dst, int nFrames)
+    {
+        Mat frame;
+        int c = (int)dst.size(), old_c = c;
+        while(nFrames == -1 || c++ < nFrames) {
+            src >> frame;
+            if (frame.empty())
+                return (c == old_c+1);
+            
+            cvtColor(frame, frame, CV_BGR2RGB);
+            
+            if (THREE_CHAN_MODE | DEBUG_MODE)
+            	dst.push_back(frame.clone());
+            else {
+				/*-----------------if using 1-chan mode, then do the colour conversion here (0)-----------------*/
+				frame.convertTo(frame, CV_64FC3);
+				if (_colourspace == "hsv")
+					cvtColor(frame, frame, CV_RGB2HSV);
+				else if (_colourspace == "ycbcr")
+					cvtColor(frame, frame, CV_RGB2YCrCb);
+				else if (_colourspace == "tsl")
+					rgb2tsl(frame, frame);
+				
+				Mat tmp = Mat::zeros(frame.rows, frame.cols, CV_64F);
+				for (i = 0; i < rows; ++i) {
+					for (j = 0; j < cols; ++j) {
+						tmp.at<double>(i, j) = frame.at<Vec3d>(i, j)[_channels_to_process];
+					}
+				}
+				
+//				Mat tmp = Mat::zeros(frame.rows, frame.cols, CV_8U);
+//				for (i = 0; i < rows; ++i) {
+//					for (j = 0; j < cols; ++j) {
+//						tmp.at<uchar>(i, j) = frame.at<Vec3b>(i, j)[_channels_to_process];
+//					}
+//				}
+				
+				dst.push_back(tmp.clone());
+            }
+        }
+        return false;
+    }
+    
+	
+	// atan2 of 2 Mats which have same size
+	// default intput/output type is double - CV_64F
+	Mat atan2Mat(const Mat &src1, const Mat &src2)
+	{
+		int nRow = src1.rows, nCol = src1.cols;
+		Mat ans = Mat::zeros(nRow, nCol, CV_64F);
+		for (int i = 0; i < nRow; ++i)
+			for (int j = 0; j < nCol; ++j)
+				ans.at<double>(i, j) += atan2(src1.at<double>(i, j), src2.at<double>(i, j));
+		return ans;
+	}
+	
+	
+	// return src.^n
+	// default intput/output type is double - CV_64F
+	Mat powMat(const Mat &src, double n)
+	{
+		Mat ans = Mat::zeros(src.rows, src.cols, CV_64F);
+		pow(src, n, ans);
+		return ans;
+	}
+	
+	
+	// return a + b
+	Mat add(const Mat &a, const Mat &b)
+	{
+		Mat ans = Mat::zeros(a.rows, a.cols, a.type());
+		add(a, b, ans);
+		return ans;
+	}
+	
+	
+	// return a .* b
+	Mat multiply(const Mat &a, const Mat &b)
+	{
+		Mat ans = Mat::zeros(a.rows, a.cols, a.type());
+		multiply(a, b, ans);
+		return ans;
+	}
+	
+	
+	// return mat .* x
+	Mat multiply(const Mat &a, double x)
+	{
+		Mat ans = Mat::zeros(a.rows, a.cols, a.type());
+		multiply(a, Mat(a.rows, a.cols, CV_64F, x), ans);
+		return ans;
+	}
+    
+    Mat read2DMatFromFile(FILE* &file, int rows, int cols)
+    {
+        Mat ans = Mat::zeros(rows, cols, CV_64F);
+        for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                fscanf(file, "%lf", &ans.at<double>(i, j));
+        return ans;
+    }
+    
+    vector<double> readVectorFromFile(FILE* &file, int n)
+    {
+        vector<double> ans;
+        double value;
+        for (int i = 0; i < n; ++i) {
+            fscanf(file, "%lf", &value);
+            ans.push_back(value);
+        }
+        return ans;
+    }
+    
+    
+    int readInt(FILE* &file)
+    {
+        int value;
+        fscanf(file, "%d", &value);
+        return value;
+    }
+    
+    
+    double readDouble(FILE* &file)
+    {
+        double value;
+        fscanf(file, "%lf", &value);
+        return value;
+    }
+}
