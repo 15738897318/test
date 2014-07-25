59ef7f361a8f869d3db74ec096c896bd9db9c9c3
Tuan-Anh Tran <tuan-anh.tran@balliol.oxon.org>
2014-07-25 15:49:37 +0700

    Addendum on attempts to use only 1 channel

--- MisfitHeartRate/MisfitHeartRate/algorithms/Eulerian/ideal_bandpassing_rev.cpp
+++ MisfitHeartRate/MisfitHeartRate/algorithms/Eulerian/ideal_bandpassing_rev.cpp
@@ -14,38 +14,64 @@ namespace MHR {
     // WL: lower cutoff frequency of ideal band pass filter
     // WH: higher cutoff frequency of ideal band pass filter
     // SAMPLINGRATE: sampling rate of SRC
-    void ideal_bandpassing(const Mat &src, Mat &dst, double wl, double wh, double samplingRate) {
-        src.convertTo(dst, CV_32F);
-        int nTime = dst.size.p[0];
-        int nRow = dst.size.p[1];
-        int nCol = dst.size.p[2];
+    void ideal_bandpassing(const vector<Mat> &src, vector<Mat> &dst, double wl, double wh, double samplingRate) {
+//        src: T*M*N*C;
+//        new src: vector<M*N*C>
         
+        // extract src info
+        int nTime = (int)src.size();
+        int nRow = src[0].rows;
+        int nCol = src[0].cols;
+        int nChannels = src[0].channels();
+        
+        // copy and convert data from src to dst (CV_32FC(nChannels))
+        Mat tmp;
+        dst.clear();
+        for (int i = 0; i < nTime; ++i)
+        {
+            src[i].convertTo(tmp, CV_32FC(nChannels));
+            dst.push_back(tmp.clone());
+        }
+        // masking indexes
         int f1 = ceil(wl * nTime/samplingRate);
         int f2 = floor(wh * nTime/samplingRate);
         int ind1 = 2*f1, ind2 = 2*f2 - 1;
         
+        if (DEBUG_MODE)
+            printf("ind1 = %d, ind2 = %d, nTime = %d\n", ind1, ind2, nTime);
+        
         // FFT
         Mat dft_out = Mat::zeros(nRow, nTime, CV_32F);
-        for (int k = 0; k < nCol; ++k) {
-            for (int i = 0; i < nTime; ++i)
-                for (int j = 0; j < nRow; ++j)
-                    dft_out.at<float>(j, i) = dst.at<float>(i, j, k);
-            dft(dft_out, dft_out, DFT_ROWS);
-            // masking
-            for (int j = 0; j < nRow; ++j) {
-                for (int i = 0; i <= ind1; ++i)
-                    dft_out.at<float>(j, i) = 0;
-                for (int i = ind2; i < nTime; ++i)
-                    dft_out.at<float>(j, i) = 0;
+        for (int channel = 0; channel < _number_of_channels; ++channel) {
+            for (int col = 0; col < nCol; ++col) {
+                for (int time = 0; time < nTime; ++time)
+                    for (int j = 0; j < nRow; ++j)
+                        if (THREE_CHAN_MODE | DEBUG_MODE)
+                        	dft_out.at<float>(j, time) = dst[time].at<Vec3f>(j, col)[channel];
+                        else
+                        	dft_out.at<float>(j, time) = dst[time].at<float>(j, col)[channel];
+                        	
+                dft(dft_out, dft_out, DFT_ROWS);
+                // masking
+                for (int j = 0; j < nRow; ++j) {
+                    for (int time = 0; time <= ind1; ++time)
+                    for (int i = 0; i <= ind1; ++i)
+                        dft_out.at<float>(j, i) = 0;
+                    for (int i = ind2; i < nTime; ++i)
+                        dft_out.at<float>(j, i) = 0;
+                }
+                // output
+                dft(dft_out, dft_out, DFT_ROWS + DFT_INVERSE + DFT_REAL_OUTPUT + DFT_SCALE);
+                for (int i = 0; i < nTime; ++i)
+                    for (int j = 0; j < nRow; ++j)
+                        if (THREE_CHAN_MODE | DEBUG_MODE)
+                        	dst[i].at<Vec3f>(j, col)[channel] = dft_out.at<float>(j, i);
+                        else
+                        	dst[i].at<float>(j, col)[channel] = dft_out.at<float>(j, i);
             }
-            // output
-            dft(dft_out, dft_out, DFT_ROWS + DFT_INVERSE + DFT_REAL_OUTPUT);
-            for (int i = 0; i < nTime; ++i)
-                for (int j = 0; j < nRow; ++j)
-                    dst.at<float>(i, j, k) = dft_out.at<float>(j, i);
         }
         
-        dst.convertTo(tmp, CV_64F);
-        dst = tmp.clone();
+        for (int i = 0; i < nTime; ++i)
+            dst[i].convertTo(dst[i], CV_64FC(nChannels));
     }
 }
