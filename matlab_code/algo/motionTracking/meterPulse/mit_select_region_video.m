4715a66bb6ef1c512363051d1412794f4496e9ab
Tuan-Anh Tran <tuan-anh.tran@balliol.oxon.org>
2015-09-18 18:17:18 -0700

    Pulsar v2.0: PCA for signal separation, PDA for beat detection

--- matlab_code/algo/motionTracking/meterPulse/mit_select_region_video.m
+++ matlab_code/algo/motionTracking/meterPulse/mit_select_region_video.m
@@ -1,6 +1,7 @@
 function [roi_streams, frameRate] = mit_select_region_video(vid, roi_params, forced_selection, cv_package)
     % Constants
     SETTLEMENT_TIME = 0.5; %seconds
+    SETTLEMENT_TIME = 5*rand; %seconds
 
     % Extract video constants
     vidHeight = vid.Height;
--- matlab_code/algo/motionTracking/meterPulse/own_peak_detection.m
+++ matlab_code/algo/motionTracking/meterPulse/own_peak_detection.m
@@ -1,49 +1,106 @@
-function signals = own_peak_detection(signals, samplingRate)
-	width_coeff = 1/2;
+function signals = own_peak_detection(signals, samplingRate, method)
 
-	for i = 1 : length(signals)
-		time_series = signals{i}.timeseries;
-		freq_pulse = signals{i}.freq_pulse;
-		
-		window_width = round(width_coeff * samplingRate / freq_pulse);
-		if length(time_series) <= window_width
-			window_width = length(time_series);
-		end
+	switch method
+		case 'naive'
+			width_coeff = 1/2;
 
-		peaks = [];
-		% Perform peak-detection at each sample
-		for j = 1 : length(time_series)
-			% Define window start & end
-			window_start = max(1, j - floor(window_width / 2));
-			window_end = min(length(time_series), window_start + window_width - 1);
-			window_start = window_end - window_width + 1;
+			for i = 1 : length(signals)
+				time_series = signals{i}.timeseries;
+				freq_pulse = signals{i}.freq_pulse;
+				
+				window_width = round(width_coeff * samplingRate / freq_pulse);
+				if length(time_series) <= window_width
+					window_width = length(time_series);
+				end
 
-			if time_series(j) == max(time_series(window_start : window_end))
-				peaks = [peaks; [j, time_series(j)]];
+				peaks = [];
+				% Perform peak-detection at each sample
+				for j = 1 : length(time_series)
+					% Define window start & end
+					window_start = max(1, j - floor(window_width / 2));
+					window_end = min(length(time_series), window_start + window_width - 1);
+					window_start = window_end - window_width + 1;
+
+					if time_series(j) == max(time_series(window_start : window_end))
+						peaks = [peaks; [j, time_series(j)]];
+					end
+				end
+
+				measured_pulse = size(peaks, 1) / length(time_series) * samplingRate;
+
+				% Calculate the heart-rate variability (hrv)
+				hrv.sdnn = [];
+				hrv.sdsd = [];
+				hrv.rmssd = [];
+				if numel(peaks) > 0
+					hrv.sdnn = std(diff(peaks(:, 1)) / samplingRate);
+					hrv.sdsd = std(diff(diff(peaks(:, 1))) / samplingRate);
+					hrv.rmssd = sqrt(sum((diff(diff(peaks(:, 1)))).^2)) / samplingRate;
+				end
+
+				signals{i}.peaks = peaks;
+				signals{i}.hrv = hrv;
 			end
-		end
 
-		% Calculate the heart-rate variability (hrv)
-		hrv.sdnn = [];
-		hrv.sdsd = [];
-		hrv.rmssd = [];
-		if numel(peaks) > 0
-			hrv.sdnn = std(diff(peaks(:, 1)) / samplingRate);
-			hrv.sdsd = std(diff(diff(peaks(:, 1))) / samplingRate);
-			hrv.rmssd = sqrt(sum((diff(diff(peaks(:, 1)))).^2)) / samplingRate;
-		end
+		case 'pda'
+			window_size_in_sec = 5;
+			window_size = round(window_size_in_sec * samplingRate);
 
-		signals{i}.peaks = peaks;
-		signals{i}.hrv = hrv;
+			for i = 1 : length(signals)
+				time_series = signals{i}.timeseries;
+				freq_pulse = signals{i}.freq_pulse;
 
-		% Plot signal and peaks
-		figure();
-		hold('on');
-		plot([0 : length(time_series) - 1] / samplingRate, time_series);
-		scatter(peaks(:, 1) / samplingRate, peaks(:, 2));
-		hold('off');
-		xlabel('Seconds');
-		legend(sprintf('HR: %g BMP', size(peaks, 1) / (length(time_series) / samplingRate) * 60), ...
-			   sprintf('HRV (SDNN): %.4g ms', hrv.sdnn));
-	end
+				max_freq = freq_pulse * 2;
+				minPeakDistance = max(round(samplingRate / max_freq), 1);
+
+				[peaks, ~, ~] = beat_counter_pda(time_series, samplingRate, 1, window_size, 0, minPeakDistance, 0);
+				peaks = peaks(:, [2 1]);
+
+				measured_pulse = size(peaks, 1) / length(time_series) * samplingRate;
+
+				% Calculate the heart-rate variability (hrv)
+				hrv.sdnn = [];
+				hrv.sdsd = [];
+				hrv.rmssd = [];
+				if numel(peaks) > 0
+					hrv.sdnn = std(diff(peaks(:, 1)) / samplingRate);
+					hrv.sdsd = std(diff(diff(peaks(:, 1))) / samplingRate);
+					hrv.rmssd = sqrt(sum((diff(diff(peaks(:, 1)))).^2)) / samplingRate;
+				end
+
+				signals{i}.peaks = peaks;
+				signals{i}.measured_pulse = measured_pulse;
+				signals{i}.hrv = hrv;
+			end;
+
+		case 'autocorr'
+			window_size_in_sec = 5;
+			window_size = round(window_size_in_sec * samplingRate);
+
+			for i = 1 : length(signals)
+				time_series = signals{i}.timeseries;
+				freq_pulse = signals{i}.freq_pulse;
+
+				max_freq = freq_pulse * 2;
+				minPeakDistance = max(round(samplingRate / max_freq), 1);
+
+				[peaks, ~, ~] = beat_counter_autocorr(time_series, samplingRate, 1, window_size, 0, minPeakDistance, 0);
+				peaks = peaks(:, [2 1]);
+
+				measured_pulse = size(peaks, 1) / length(time_series) * samplingRate;
+
+				% Calculate the heart-rate variability (hrv)
+				hrv.sdnn = [];
+				hrv.sdsd = [];
+				hrv.rmssd = [];
+				if numel(peaks) > 0
+					hrv.sdnn = std(diff(peaks(:, 1)) / samplingRate);
+					hrv.sdsd = std(diff(diff(peaks(:, 1))) / samplingRate);
+					hrv.rmssd = sqrt(sum((diff(diff(peaks(:, 1)))).^2)) / samplingRate;
+				end
+
+				signals{i}.peaks = peaks;
+				signals{i}.measured_pulse = measured_pulse;
+				signals{i}.hrv = hrv;
+			end;
 end
